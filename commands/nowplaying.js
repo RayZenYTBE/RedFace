95e379afbac638685734e0309dfdb45a6e1d015a
AtMostFearDevs <87268716+RayZenYTBE@users.noreply.github.com>
2021-12-11 18:34:53 +0700

    RedFace Aura Injected

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -20,7 +20,7 @@ module.exports = {
     let nowPlaying = new MessageEmbed()
       .setTitle(i18n.__("nowplaying.embedTitle"))
       .setDescription(`${song.title}\n${song.url}`)
-      .setColor("#F8AA2A")
+      .setColor("#FF0000")
       .setAuthor(message.client.user.username);
 
     if (song.duration > 0) {
