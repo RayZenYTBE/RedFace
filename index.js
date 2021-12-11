8400e64e1401fcbf58875bf53946d8bd31999a2f
AtMostFearDevs <87268716+RayZenYTBE@users.noreply.github.com>
2021-12-11 19:01:14 +0700

    RedFace Aura Injected

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const client = new Client({
   restTimeOffset: 0
 });
 
+const express = require('express')//important package
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Its Working! Copy this link above and navigate to uptimerobot.com'))//If you see this message above the console, you're good to go!
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+
 client.login(TOKEN);
 client.commands = new Collection();
 client.prefix = PREFIX;
